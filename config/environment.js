7902561fa2e9e00cbc859925d78f4cb0ab2587e0
sandydoo <7572407+sandydoo@users.noreply.github.com>
2020-05-05 13:06:45 +0300

    Set up google maps

--- config/environment.js
+++ config/environment.js
@@ -20,6 +20,12 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    'ember-google-maps': {
+      language: 'en',
+      libraries: ['geometry'],
+      key: process.env.GOOGLE_MAPS_API_KEY
     }
   };
 
